275cfba51ffb00709ade7c98ac4d8167b2be5708
white-spring <231016511@qq.com>
2021-12-14 20:34:30 +0800

    init;

---
+++ spring-boot/src/main/java/com/white/boot/WhiteApplication.java
@@ -0,0 +1,14 @@
+package com.white.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author baichunqiang
+ */
+@SpringBootApplication
+public class WhiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteApplication.class, args);
+    }
+}
